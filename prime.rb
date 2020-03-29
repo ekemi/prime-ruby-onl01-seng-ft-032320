b785ea1228813d0fa4b6c5a481052ee4a10193fc
edmundo papi <edmundopierre@yahoo.com>
2020-03-29 05:06:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+# # Add  code here!
+ def prime?(number)
+#
+
+if number < 2
+  return false
+
+else
+
+  (2..number-1).to_a.each do |value|
+
+     if value % 2 != 0 && value > 0
+#
+        return true
+
+    elsif number < 0
+
+       return false
+
+    else
+        return false
+end
+end
+ end
+ end
