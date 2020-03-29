cb1f76edcf4b924401d7f9ff30dee9666ef30d49
edmundo papi <edmundopierre@yahoo.com>
2020-03-29 05:19:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 
 return false if number <2
 return true if number==2
-(2..number-1).each{ |value| return false if value ==0}
+(2..number-1).each{ |value| return false if number%value ==0}
 
 true
 end
